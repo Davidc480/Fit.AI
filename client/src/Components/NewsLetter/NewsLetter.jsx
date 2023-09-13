e1d15ed69d1048a0b2c1a882fb2eb7c4883717e1
Davidc480 <Davidcalle.480@gmail.com>
2023-09-13 18:27:39 -0500

    Trabajando en el NeswsLetter

---
+++ client/src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,19 @@
+const NewsLetter =()=>{
+    return (
+        <div class="text-white text-center font-montserrat p-20">
+            <div>
+                <h4 class=" text-5xl not-italic font-bold leading-normal inline relative">SUSCRÍBETE
+                <span class="absolute w-1/2 h-1 bg-blue-600 bottom-[-0.3rem] left-1/4"></span>
+                </h4>
+            </div>
+            <div class="mt-20">
+                <p class="text-2xl">¡Únete a nuestra newsletter semanal en FitAI y descubre un mundo de bienestar!</p>
+                <p class="text-2xl">Recibe consejos, dietas personalizadas y mucho más para alcanzar tus metas </p>
+                <p class="text-2xl">fitness. ¡Únete a nuestra comunidad saludable hoy mismo y comienza a </p>
+                <p class="text-2xl">transformar tu vida!</p>
+            </div>
+        </div>
+    )
+}
+
+export default NewsLetter
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -2,6 +2,7 @@ import Header from "@/Components/Header/Header";
 import Carrousel from "@/Components/Carrousel/Carrousel";
 import CarrouselW from "@/Components/Carrousel/CarrouselW";
 import RouteCard from "@/Components/RouteCard/RouteCard";
+import NewsLetter from "@/Components/NewsLetter/NewsLetter";
 import imgMans from "@/app/img/imgMan";
 import imgWomans from "./img/imgWomans";
 
@@ -19,10 +20,13 @@ export default function Home() {
           <CarrouselW images={imgWomans} />
         </div>
       </div>
-      <div class="w-screen h-screen float flex-col items-center justify-center bg-black p-20">
+      <div class="w-screen h-5/6 float flex-col items-center justify-center bg-black p-20">
         <div class="ml-[14%]">
           <RouteCard />
         </div>
+      </div>
+      <div class="w-screen h-screen shrink-0 flex-col items-center justify-center bg-gradient-radial">
+        <NewsLetter />
       </div>
     </main>
   );
