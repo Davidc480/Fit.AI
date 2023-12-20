83f9aab14f6e2c58e20ec69acad5e45c5c17e33b
David <Davidcalle.480@gmail.com>
2023-12-20 16:40:59 -0600

    Arreglos

--- client/src/Components/FormIa/Page_2.jsx
+++ client/src/Components/FormIa/Page_2.jsx
@@ -3,8 +3,21 @@ import ButtonFormIa from "../ButtonFormIa/ButtonFormIa";
 const PageTwo = ()=>{
     return(
         <div class="flex flex-col relative h-full p-5">
-            <div> 
-                <h1>Página 2</h1>
+           <div class="text-center text-white font-inter text-2xl xl:text-4xl 2xl:text-6xl not-italic font-bold leading-normal mt-24 xl:mt-12">
+                <p>¡Felicidades! ahora estas a un <span class="text-darkRose">paso</span> más cerca de tu <span class="text-darkRose">meta</span></p>
+            </div>
+            <div>
+                <div>
+                    <p>Edad</p>
+                </div>
+                <div>
+                    <input type="number" />
+                </div>
+            </div>
+            <div>
+                <div>
+                    <p>Genero</p>
+                </div>
             </div>
             <div class="absolute bottom-0 left-0 right-0 flex justify-center">
                     <ButtonFormIa />
--- client/src/Components/MenuForm/MenuForm.jsx
+++ client/src/Components/MenuForm/MenuForm.jsx
@@ -6,7 +6,9 @@ const MenuForm = ()=>{
     return(
         <div>
             <div class="w-32 h-14 shrink-0 text-darkRose font-inter text-4xl xl:text-5xl not-italic font-bold leading-normal absolute top-6 xl:top-9 left-9 xl:left-14">
-                FitAI
+                <p class="cursor-pointer" title="Fit.AI">
+                Fit.AI
+                </p>
             </div>
             <div>
                 <ButtonX />
