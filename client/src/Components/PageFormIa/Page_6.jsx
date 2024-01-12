b02a5bceb23dd21eee2d9b941d684103a7c3feda
David <Davidcalle.480@gmail.com>
2024-01-11 23:45:11 -0600

    pagina 6

--- client/src/Components/PageFormIa/Page_6.jsx
+++ client/src/Components/PageFormIa/Page_6.jsx
@@ -1,15 +1,46 @@
+'use client'
+
 import ButtonFormIa from "../ButtonFormIa/ButtonFormIa";
+import { useState, useEffect } from "react";
 
 const PageSix = ()=>{
+
+    const [valueInput, setValueInput] = useState({enabled: false, diasEntrenamiento: "", tiempoLibre: "", preferenciaEntrenamiento: "",});
+    const [error, setError] = useState("")
+
+
+    const handleChange = (e) =>{
+        const { name, value } = e.target;
+
+        if(name === "diasEntrenamiento"){
+            if(value >= 3 && value <= 7){
+                setValueInput(prevState=>({...prevState, diasEntrenamiento: `Quiero entrenar ${value} dias a la semana`}))
+                setError("")
+            } else {
+                setError("Por favor, elige entre 3 y 6 días para tu rutina semanal de entrenamiento.")
+            }
+        }
+
+    }
+
+    useEffect(() => {
+        if (valueInput.diasEntrenamiento && valueInput.tiempoLibre && valueInput.preferenciaEntrenamiento) {
+          setValueInput(prevState => ({ ...prevState, enabled: true }));
+        } else {
+          setValueInput(prevState => ({ ...prevState, enabled: false }));
+        }
+      }, [valueInput.diasEntrenamiento, valueInput.tiempoLibre, valueInput.preferenciaEntrenamiento]);
+
     return(
         <div class="flex flex-col relative h-full p-5 ">
             <div class="xl:flex xl:translate-x-7 2xl:translate-x-0">
                 <div class="xl:w-[25rem] 2xl:w-[43rem]">
                     <p class="font-montserrat text-xl xl:text-2xl 2xl:text-5xl not-italic font-bold leading-normal text-center text-lightBlue">¿Cuantos dias a la semana puedes entrenar?</p>
                     <div class="flex items-end justify-center mt-4 xl:mt-6 2xl:mt-12">
-                        <input type="number"  class="no-spinners 2xl:w-32 -translate-y-1  text-center w-20  bg-transparent border-b-2 xl:border-b-4 border-b-white focus:ring-0 focus:outline-none text-lg xl:text-2xl 2xl:text-4xl" />
+                        <input type="number" name="diasEntrenamiento" onChange={handleChange} class="no-spinners 2xl:w-32 -translate-y-1  text-center w-20  bg-transparent border-b-2 xl:border-b-4 border-b-white focus:ring-0 focus:outline-none text-lg xl:text-2xl 2xl:text-4xl" />
                         <label class="ml-3 2xl:ml-4 font-montserrat text-sm xl:text-base 2xl:text-3xl font-bold leading-normal">Días</label>
                     </div>
+                        <label class="mt-2 flex text-center text-red-500 font-montserrat font-bold leading-normal not-italic">{error}</label>
                 </div>
                 <div class="mt-8 xl:mt-0 xl:w-[30rem] 2xl:w-[58rem]">
                     <p class="font-montserrat text-xl xl:tex2xl 2xl:text-5xl not-italic font-bold leading-normal text-center text-lightBlue">¿Cuánto tiempo libre tienes para hacer ejercicio cada día?</p>
