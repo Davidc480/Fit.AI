48b42a8f4efc1aab2a659d28a82dd5f1d9c6801d
David <Davidcalle.480@gmail.com>
2024-01-16 21:52:27 -0600

    formulario finalizado

--- client/src/Components/PageFormIa/Page_5.jsx
+++ client/src/Components/PageFormIa/Page_5.jsx
@@ -8,6 +8,7 @@ const PageFive = ()=>{
     const [valueInput, setValueInput] = useState({enabled: false, plazo: "",});
     const [isRadioChecked, setIsRadioChecked] = useState(false)
     const [isTextChecked, setIsTextChecked] = useState(false)
+    const [isTextCheckedSi, setIsTextCheckedSi] = useState(false)
     const [error, setError] = useState("")
 
 
@@ -26,8 +27,6 @@ const PageFive = ()=>{
             setValueInput(prevState=>({...prevState, plazo: value}))
             setError("")
         }
-
-        
     }
 
     useEffect(() => {
@@ -39,6 +38,9 @@ const PageFive = ()=>{
         if(isTextChecked){
             document.getElementById("text").value = "";
             setError("")
+        }
+        if(isRadioChecked){
+            setIsRadioChecked(false)
         }
       }, [valueInput.plazo, isTextChecked]);
 
@@ -55,7 +57,7 @@ const PageFive = ()=>{
             </div>
             <div class="flex mt-8 xl:mt-14 2xl:mt-24 ml-14 xl:ml-20 gap-10">
                 <div>
-                    <input type="radio" onClick={()=>{isRadioChecked ? setIsRadioChecked(false) : null; !isRadioChecked ?  setIsTextChecked(true): setIsTextChecked(false)}} name="plazo" class="cursor-pointer mr-2 xl:w-4 xl:h-4 2xl:w-8 2xl:h-8" />
+                    <input type="radio" onChange={handleChange} checked={isTextCheckedSi} onClick={()=>{setIsTextCheckedSi(!isTextCheckedSi); isTextCheckedSi ? setIsRadioChecked(false) : null; isTextCheckedSi ?  setIsTextChecked(true): setIsTextChecked(false)}} name="plazo" class="cursor-pointer mr-2 xl:w-4 xl:h-4 2xl:w-8 2xl:h-8" />
                     <label htmlFor="plazo" class="font-montserrat text-sm xl:text-base 2xl:text-3xl font-bold leading-normal">Sí</label>
                 </div>
                 <div class="-translate-y-1 xl:-translate-y-2 2xl:-translate-y-3">
